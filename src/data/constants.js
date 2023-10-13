e19f753d44ff8726feaa4f6bc104929361674f06
Your Name <your email>
2023-10-13 22:44:12 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,154 @@
+export const Bio = {
+    name: "Reshma S",
+    roles: [
+      "MERN FullStack Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/SReshma2412",
+    resume:
+     "https://drive.google.com/drive/folders/1iTNXPOFDhb-4NlPPexKvKBztNudDgZqH" ,
+    linkedin:"https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit" ,
+    twitter: "https://twitter.com/ReshmaS1787100",
+    insta: "https://www.instagram.com/resh_kutty76/",
+    facebook:"https://www.facebook.com/profile.php?id=100087617051488" ,
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+    
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+       
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+     
+       
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+       
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+       
+       
+      ],
+    },
+    
+         
+       
+
+  ];
+  
+  export const experiences = [
+     
+    {
+      id: 0,
+      role: "MERN FullStack Intern",
+      company: "Ethunus",
+      date: "August 2023 - Present",
+      desc: "Working on the frontend of the web application using ReactJS",
+      skills: [
+        "ReactJS",
+        "NodeJs",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "MongoDB"
+      ],
+     
+    },
+   
+   
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      school: "Vellore Institute of Technology,Vellore",
+      date: "Oct 2021 - Sep 2025",
+      grade: "8.59 CGPA",
+      desc: "I am currently pursuing a BTech Information Technology at Vellore Institute of Technology,Vellore. I have completed 5 semesters and have a CGPA of 8.59. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech Infromation Technology",
+    },
+    {
+      id: 1,
+      school: "Government Higher Secondary School,Thirumullaivasal",
+      date: "Apr 2020 - Apr 2021",
+      grade: "92.2%",
+      desc: "I completed my class 12 high school education at Government Higher Secondary School,Thirumullaivasal  where I studied Maths-Biology.",
+      degree: "Maths-Biology",
+    },
+    {
+      id: 2,
+      school: "Government Higher Secondary School,Thirumullaivasal",
+      date: "Apr 2018 - Apr 2019",
+      grade: "92.3%",
+      desc: "I completed my class 10 education at Government Higher Secondary School,Thirumullaivasal,  where I studied Maths-Biology",
+      degree: "Maths-Biology",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Automated Toll Gate System in IOT",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "An automated toll gate system is a technology-driven solution that streamlines the process of collecting tolls from vehicles by employing electronic toll collection methods, reducing traffic congestion and enhancing efficiency. It typically involves RFID tags, cameras, and payment gateways to ensure swift and convenient toll payment.",
+
+    },
+    {
+      id: 10,
+      title: "Online Hotel Management System",
+      date: "Jun 2023 - december 2023",
+      description:
+        "An online hotel management system is a software application designed to streamline and automate various operational aspects of a hotel or hospitality establishment. It serves as a centralized platform that enables hotel staff to efficiently manage their day-to-day tasks, while also providing a user-friendly experience for guests to make reservations, check-in, and access information about the hotel's services and amenities.",
+
+    },
+    
+   
+  ];
+  
